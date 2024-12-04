26a3e78b7b4074eb8edced4cdb330063f13ab235
Victor <victorgrinan@gmail.com>
2024-12-04 18:02:21 +0200

    New some

--- src/components/some/Some.jsx
+++ src/components/some/Some.jsx
@@ -33,6 +33,9 @@ const Some = () => {
     <div className='some-container'>
        <i className="fab fa-tiktok some-icon"></i>
     </div>
+
+    <iframe src="https://app.acuityscheduling.com/schedule.php?owner=34124582&ref=embedded_csp" title="Schedule Appointment" width="100%" height="800" frameBorder="0"></iframe>
+    <script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
     
 </div>
   )
