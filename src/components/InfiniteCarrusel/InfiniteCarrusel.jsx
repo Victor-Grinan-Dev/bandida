d31fd917ecb4567c05de899da03429d649257b59
Victor <victorgrinan@gmail.com>
2024-12-05 14:02:24 +0200

    update the carrusel

--- src/components/InfiniteCarrusel/InfiniteCarrusel.jsx
+++ src/components/InfiniteCarrusel/InfiniteCarrusel.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import test1 from '../../assets/images/tatoos/a test-1.jpg'
-import test2 from '../../assets/images/tatoos/a test-2.jpg'
-import test3 from '../../assets/images/tatoos/a test-3.jpg'
-import test4 from '../../assets/images/tatoos/a test-4.jpg'
-import test5 from '../../assets/images/tatoos/a test-5.jpg'
-import test6 from '../../assets/images/tatoos/a test-6.jpg'
-import test7 from '../../assets/images/tatoos/a test-7.jpg'
-import test8 from '../../assets/images/tatoos/a test-8.jpg'
+import test1 from '../../assets/images/all pics/pic001.jpg'
+import test2 from '../../assets/images/all pics/pic002.jpg'
+import test3 from '../../assets/images/all pics/pic003.jpg'
+import test4 from '../../assets/images/all pics/pic004.jpg'
+import test5 from '../../assets/images/all pics/pic005.jpg'
+import test6 from '../../assets/images/all pics/pic006.jpg'
+import test7 from '../../assets/images/all pics/pic007.jpg'
+import test8 from '../../assets/images/all pics/pic008.jpg'
 
 import AppLink from '../appLink/AppLink';
 
@@ -19,7 +19,7 @@ const InfiniteCarrusel = () => {
                 <img src={test1} alt="tattoo" />
             </div>
             <div className='carrusel-item item2'>
-                <img src={test2} alt="tattoo" />
+                <img src={test2} alt="tattoo" /> 
             </div>
             <div className='carrusel-item item3'>
                 <img src={test3} alt="tattoo" />
@@ -43,9 +43,16 @@ const InfiniteCarrusel = () => {
 
 
         <div className='app-link-container'>
-            <AppLink caption="Lisää" linkTo="/gallery"/>
+            <AppLink caption="katso Lisää kuvia" linkTo="/gallery"/>
         </div>
 
+        <div className="test" style={{textAlign:"center", lineHeight:2.5}}>
+
+            <h1>My Style</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eum velit debitis? Excepturi ullam et cum quasi suscipit veritatis repudiandae rem vero blanditiis sapiente. Illo perspiciatis corporis quidem eos laborum vel sequi debitis veniam iure numquam, voluptatibus aliquid rem, nesciunt cupiditate laboriosam dolor ab consequatur dolore nostrum suscipit natus non possimus maiores? Totam enim repudiandae impedit, quos eos dolore qui illo accusamus ad eligendi nesciunt quo atque excepturi veniam officiis quis laborum? Vero aliquam quibusdam nihil rem non. Ex nam fugit in, expedita nihil sint suscipit iusto sed provident ratione eos dicta cum dignissimos sit nesciunt ea, laudantium, voluptas similique.
+            </p>
+        </div>
   </section>
   )
 }
