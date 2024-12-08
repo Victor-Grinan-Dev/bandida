e5e83345f04e1d48ed62e486aa1968071cca93cc
Victor <victorgrinan@gmail.com>
2024-12-08 08:46:59 +0200

    Navegator

--- src/components/navegator/Navegator.jsx
+++ src/components/navegator/Navegator.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
+import AppLink from '../../components/appLink/AppLink';
 
 const Navegator = () => {
+      const navBarRef = useRef(null);
+
+    useEffect(() => {
+        const navBar = navBarRef.current;
+        const originalPosition = navBar.offsetTop;
+    
+        const handleScroll = () => {
+          if (window.scrollY >= originalPosition) {
+            navBar.classList.add("sticky");
+          } else {
+            navBar.classList.remove("sticky");
+          }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+    
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
+
   return (
-    <div className='navegator'>
-        <a href="#services">Palvelut</a>
-        <a href="#porfolio">Töitäni</a>
-        <a href="#contact">Ota Yhteys</a>
-    </div>
+    <div className="nav-bar" id="nav-bar" ref={navBarRef}>
+      <div className="link-wrapper">
+          <AppLink caption="Kotisivu"  />
+          <AppLink caption="Info"  />
+          <AppLink caption="Studio"  />
+          <AppLink caption="Galleria"  />
+          <AppLink caption="Tyyliestäni"  />
+          <AppLink caption="Ajanvaraus"  />
+          <AppLink caption="Ota Yhteys"  />
+      </div>
+  </div>
   )
 }
 
