4028a64d97f2ad04e0ee0ab6f9bed6fe46bcdaf2
Victor <victorgrinan@gmail.com>
2024-12-09 16:28:07 +0200

    modal menu ready

--- src/components/menuModal/MenuModal.jsx
+++ src/components/menuModal/MenuModal.jsx
@@ -3,19 +3,27 @@ import AppLink from '../appLink/AppLink';
 import { APPLINKS } from '../../settings/setings';
 import { useDispatch } from 'react-redux';
 import { toggleIsModal } from '../../app/appSlice';
+import { useNavigate } from 'react-router-dom';
 
 const MenuModal = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate()
+
+    const handleLinks = (endpoint) => {
+        dispatch(toggleIsModal());
+        navigate(endpoint)
+    }
 
   return (
     <div className='menu-modal'>
         <div className="links-wrapper">
 
             {APPLINKS.map((link, idx)=>(
-                <div className="link-wraper" key={idx} onClick={()=>dispatch(toggleIsModal())}>
+                <div className="link-wraper" key={idx} onClick={()=>handleLinks(link.linksTo)}>
                     <AppLink 
                         caption={link.caption} 
-                        linkTo={link.linkTo}
+                        linkTo={link?.linksTo}
+                        idLink={link.type === "a"}
                     />
                 </div>
             ))}
