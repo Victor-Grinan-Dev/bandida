bf20cf2093aaf94ef3457d903726fd44efe2870e
Victor <victorgrinan@gmail.com>
2024-12-11 14:31:28 +0200

    delete google icons

--- src/components/some/Some.jsx
+++ src/components/some/Some.jsx
@@ -5,14 +5,15 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 const Some = () => {
   return (
    <div className="some">
-      <a 
+      {/* <a 
          className="some-item" 
          target="_blank" 
          rel="noopener noreferrer" 
          href="https://mail.google.com/mail/?view=cm&to=paulaannelica.tattoo@gmail.com"
          >
       <i className="fas fa-envelope some-icon"></i>
-      </a>
+      </a> */}
+      <Link to="/booking"><i className="fas fa-envelope some-icon"/></Link>
       <a target='blank' href='https://www.facebook.com/profile.php?id=61550417768685&mibextid=LQQJ4d'><i className="fab fa-facebook some-icon"></i></a>
       <a target='blank' href='https://www.instagram.com/paulaannelica.tattoo/'><i className="fab fa-instagram some-icon"></i></a>
       <a target='blank' href='https://www.tiktok.com/@paulaannelica.tattoo'><i className="fab fa-tiktok some-icon"></i></a>
--- src/pages/bookings/Bookings.jsx
+++ src/pages/bookings/Bookings.jsx
@@ -1,7 +1,14 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import AppLink from '../../components/appLink/AppLink';
+import PicLoader from '../../components/picLoader/PicLoader';
 
+
+const copiedIcon = () => {
+  return (
+    <div>copied</div>
+  )
+}
 
 const Bookings = () => {
   const { pathname } = useLocation();
@@ -10,38 +17,73 @@ const Bookings = () => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  return (
-    <div style={{color:"white"}}>
+  const copyToClipboard = (copiedText) => {
 
-        <div className="tabs-bar" >
+    navigator.clipboard.writeText(copiedText)
+      .then(() => alert("Phone number copied to clipboard!"))
+      .catch((err) => console.error("Failed to copy text: ", err));
+  };
+
+  return (
+    <div className='boobkings' style={{color:"white"}}>
+
+        <div className="boobkings__tabs-bar" >
 
           <div className="link-wrapper">
               <AppLink caption="Takaisin" linkTo="/"/>
           </div>
 
-          <h4 style={{color:"white"}}>Bookings</h4>
+        </div>
 
-          <div className="content-wrapper">
+        <div className="booking-text">
+          <h4 style={{color:"white"}}>Ajanvaraus:</h4>
+          
+        </div>
+
+
+        <div className="content-wrapper">
 
           <div className="content-wrapper__column-a">
-            some image i guess
+            <PicLoader pic="paula_medium" style={{objectFit:"cover"}} />
           </div>
 
           <div className="content-wrapper__column-b">
-            <div className="content-item">
-              <span>Icon</span>
-              <p>content text</p>
+
+            <div className="content-item" >
+              <p>Työskentelemme varausjärjestelmän parissa, mutta toistaiseksi varaukset voi tehdä sähköpostitse, puhelimitse tai WhatsAppin kautta. Kiitos.</p>
             </div>
 
-            <div className="content-item">
-              <span>Icon</span>
-              <p>content text </p>
+            <div className="content-item" >
+              <div className='content-item__head'>              
+                <i className="fas fa-envelope some-icon"/>
+                <p>paulaannelica.tattoo@gmail.com</p>
+              </div>
+
+              <i className="fa-regular fa-copy" onClick={()=>copyToClipboard("paulaannelica.tattoo@gmail.com")} />
             </div>
-          </div>
-
-
             
+            <div className="content-item" >
+              <div className='content-item__head'>              
+                <i className="fas fa-phone some-icon"/>
+                <span>Puh.</span>
+                <p>+358 45 78392909</p>
+              </div>
+
+              <i className="fa-regular fa-copy" onClick={()=>copyToClipboard("+358 45 78392909")} />
+            </div>
+
+            <div className="content-item" >
+              <div className='content-item__head'> 
+                <i className="fab fa-whatsapp some-icon"/>
+                <span>Whatsapp</span>
+                <p>+358 45 78392909</p>
+              </div>
+              <i className="fa-regular fa-copy" onClick={()=>copyToClipboard("+358 45 78392909")} />
+
+            </div>
+
           </div>
+
       </div>
       
     </div>
@@ -49,3 +91,7 @@ const Bookings = () => {
 }
 
 export default Bookings;
+
+
+
+
