41f9de5c40688688b2dc824c97246bb921e61d6d
Victor <victorgrinan@gmail.com>
2024-12-11 10:57:13 +0200

    create bookings

--- src/pages/bookings/Bookings.jsx
+++ src/pages/bookings/Bookings.jsx
@@ -11,13 +11,39 @@ const Bookings = () => {
   }, [pathname]);
 
   return (
-    <div>
-        <div className="tabs-bar">
+    <div style={{color:"white"}}>
+
+        <div className="tabs-bar" >
+
           <div className="link-wrapper">
               <AppLink caption="Takaisin" linkTo="/"/>
           </div>
+
+          <h4 style={{color:"white"}}>Bookings</h4>
+
+          <div className="content-wrapper">
+
+          <div className="content-wrapper__column-a">
+            some image i guess
+          </div>
+
+          <div className="content-wrapper__column-b">
+            <div className="content-item">
+              <span>Icon</span>
+              <p>content text</p>
+            </div>
+
+            <div className="content-item">
+              <span>Icon</span>
+              <p>content text </p>
+            </div>
+          </div>
+
+
+            
+          </div>
       </div>
-      <h4 style={{color:"white"}}>Bookings</h4>
+      
     </div>
   )
 }
