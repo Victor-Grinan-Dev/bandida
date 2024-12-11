6fe9610f5e15ef34d3e7e62623d6d332a68c4c8d
Victor <victorgrinan@gmail.com>
2024-12-11 16:07:31 +0200

    transparent section

--- src/components/transparentSection/TransparentSection.jsx
+++ src/components/transparentSection/TransparentSection.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const TransparentSection = () => {
+const TransparentSection = ({ children, className }) => {
   return (
-    <section className='transparent-section'>TransparentSection</section>
+    <section className={`transparent-section ${className}`}>{children}</section>
   )
 }
 
