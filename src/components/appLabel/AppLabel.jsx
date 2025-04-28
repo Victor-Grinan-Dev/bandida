9edb630cdbfba5b81031ae70df3e67649f81c37c
Victor <victorgrinan@gmail.com>
2025-04-28 11:38:37 +0300

    filter spl characeter fro translation in appLabel

--- src/components/appLabel/AppLabel.jsx
+++ src/components/appLabel/AppLabel.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import { translate } from '../../translation/translator';
 import { useSelector } from 'react-redux';
 
-const AppLabel = ({caption, extraStyle}) => {
-    const currentLang = useSelector((state) => state.app.currentLang);
-  return (
-    <div className='app-label' style={extraStyle}>{translate(caption, currentLang)}</div>
-  )
-}
+const AppLabel = ({ caption, extraStyle }) => {
+  const currentLang = useSelector((state) => state.app.currentLang);
 
-export default AppLabel
+  const isNotSpecialCharacter = str => /^[a-zA-Z0-9]+$/.test(str);
+
+  if (!isNotSpecialCharacter(caption)) return (
+    <div className='app-label' style={extraStyle}>
+      {caption}
+    </div>
+  );
+
+  return (
+    <div className='app-label' style={extraStyle}>
+      {translate(caption, currentLang)}
+    </div>
+  );
+};
+
+export default AppLabel;
