e4cdf9e17048c9dba62b52c9b07127de197c81f1
Victor <victorgrinan@gmail.com>
2025-05-26 11:52:41 +0300

    pass children in applink

--- src/components/appLink/AppLink.jsx
+++ src/components/appLink/AppLink.jsx
@@ -4,18 +4,24 @@ import { translate } from '../../translation/translator';
 import { useSelector } from 'react-redux';
 
 const AppLink = (props) => {
-    const {linkTo, caption, idLink=false, active=false, extraStyle} = props;
+    const {linkTo, caption, idLink=false, active=false, extraStyle, children, fx} = props;
     const currentLang = useSelector((state) => state.app.currentLang);
-
-
-    if(idLink){
+  
+    if (children) {
+      return (
+        <div className={`app-link ${active && "active"}`} style={extraStyle} onClick={fx} >
+          {translate(caption, currentLang)}
+          {children}
+        </div>
+      )
+    }
+    else if(idLink){
       return (
         <div className={`app-link ${active && "active"}`} >
             <HashLink to={`${linkTo}`} style={extraStyle} > { translate(caption, currentLang) } </HashLink>
         </div>
       )
     }
-
     return (
       <div className={`app-link ${active && "active"}` }>
           <Link aria-label={`Go To ${caption}`} to={linkTo} style={extraStyle} > { translate(caption, currentLang) } </Link>
