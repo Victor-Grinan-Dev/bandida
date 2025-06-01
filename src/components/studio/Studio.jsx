50dbe4ca7cdca87d69a6e00f8b632089b42754e2
Victor <victorgrinan@gmail.com>
2025-06-01 16:15:03 +0300

    change text studioSection

--- src/components/studio/Studio.jsx
+++ src/components/studio/Studio.jsx
@@ -11,35 +11,16 @@ const Studio = () => {
   const currentLang = useSelector((state) => state.app.currentLang);
   
   return (
-    <section className='studio' id='studio'>
+    <section className='studio' id='studio-section'>
 
       <div className="panel-1">
         <div className="text-area">
-          <p>Pieni, mutta persoonallinen studio, jossa taide kohtaa ammattitaidon.</p> 
+          <p>Pieni, mutta persoonallinen tatuointistudio.</p> 
+          <br />
+          <p>Pieni ja persoonallinen studiomme sijaitsee Porvoonkatu 14:ssä, Helsingin Alppilassa, aivan Linnanmäen naapurissa. Meille pääset nopeasti esimerkiksi Kalliosta, Pasilasta ja keskustasta.</p>
+          <br />
           <p>Täällä tehdään kauneutta ja taidetta tunteella ja taidolla!</p>
           <ReadMore caption="lue lisää" linkTo="/studio"/>
-
-          {/* <p>Studio Inky 11 on vuonna 2024 perustettu tatuointeihin ja kestopigmentointeihin keskittynyt pieni studio Helsingin Alppilassa, aivan Linnanmäen kupeessa. Olipa ajatuksenasi pieni siro tatuointi tai isompi kokonaisuus mustaharmaana tai väreissä, toteutetaan nämä turvallisesti, huolella ja asiakkaan toiveita kuunnellen. Olet lämpimästi tervetullut juuri sellaisena kuin olet. </p> 
-
-          <br />
-            
-          <p>
-            Studio sijaitsee aivan Linnanmäen kupeessa Alppilassa. 
-            Raitiovaunut 3 ja 4 pysähtyy suoraan studion eteen. Pysäkki on ”Alppila”.
-            Sturenkadulta kulkee useita raitiovaunuja ja busseja ja siitä kävelee pari minuuttia studiolle.
-            Pasilasta matkaa on 2km ja bussi nro 23 tulee Pasilasta suoraan Sturenkadulle. 
-            Autolle pysäköintipaikkoja on paljon kadun varsilla ja on ilmaista la-su, muina aikoina maksullista. Linnanmäen parkkialue on hiukan kalliimpi kuin kadun varret. 
-          </p> */}
-
-          {/*
-          
-          <p>Studiomme sijaitsee Porvoonkatu 14:ssä, Helsingissä, aivan Linnanmäen naapurissa. Se on suunniteltu tarjoamaan rauhallinen, rento ja turvallinen ympäristö niin tatuoinneille kuin kestopigmentoinneillekin.</p> 
-          <p>Panostamme puhtauteen, laatuun ja asiakaskokemukseen – jokainen asiakas kohdataan yksilöllisesti ja suurella huolella. Käytössämme ovat korkealuokkaiset välineet ja tuotteet, ja hygienia on meille ensiarvoisen tärkeää. Tavoitteenamme on kestävä lopputulos, joka on juuri sinun toiveidesi mukainen.</p> 
-          <p>Olitpa ensikertalainen tai kokenut tatuointien tai pigmentointien ystävä, olet lämpimästi tervetullut.</p> 
-          <p>Varaa aika tai tule paikan päälle piipahtamaan – tehdään yhdessä jotain upeaa.</p> 
-          <p>Täällä tehdään kauneutta ja taidetta tunteella ja taidolla!</p>
-          */}
-          
         </div>
       </div>
 
