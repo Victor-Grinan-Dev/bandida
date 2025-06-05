959680719b568409ca3040063920003eff829480
Victor <victorgrinan@gmail.com>
2025-06-05 10:10:05 +0300

    fix: Update copyright year and symbol in Legal component

--- src/components/legal/Legal.jsx
+++ src/components/legal/Legal.jsx
@@ -2,7 +2,7 @@
 const Legal = () => {
   return (
     <div className="legal">
-        <p>All rights Reserved {new Date().getFullYear()} Inky11&reg;</p>
+        <p>All rights Reserved 2024 Inky11&copy;</p> 
     </div>
   )
 }
