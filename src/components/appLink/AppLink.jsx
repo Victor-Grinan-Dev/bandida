bbef69efaa27767fa40e5c92da4c4c1a85122628
Victor <victorgrinan@gmail.com>
2025-06-25 16:30:29 +0300

    fix: Update aria-label in AppLink component for consistency and clarity

--- src/components/appLink/AppLink.jsx
+++ src/components/appLink/AppLink.jsx
@@ -26,7 +26,7 @@ const AppLink = (props) => {
     }
     return (
       <div className={`app-link ${active && "active"}` }>
-          <Link aria-label={`Go To ${capitalCaption}`} to={linkTo} style={extraStyle} > {capitalize(translate(caption, currentLang || currentLang))} </Link>
+          <Link aria-label={`Go To ${caption}`} to={linkTo} style={extraStyle} > {capitalize(translate(caption, currentLang || currentLang))} </Link>
       </div>
     )
 }
--- src/helper/capitalizer.js
+++ src/helper/capitalizer.js
@@ -1,4 +1,7 @@
 export const capitalize = (text) => {
+    // console.log("Capitalizing text:", text);
       if (!text) return text;
+      else if (text === true) return text;
+
   return text[0].toUpperCase() + text.slice(1); 
 }
